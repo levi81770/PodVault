c5c7662da52733961885c2c8d3ba3e78389a3dc7
levi81770 <levi77081@gmail.com>
2026-06-30 23:36:04 -0400

    Add authentication

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('logout/', views.LogoutUser.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
     path('podcasts/', views.PodcastList.as_view(), name='podcast_list'),
     path('podcasts/<int:pk>/', views.PodcastDetail.as_view(), name='podcast_detail'),
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -1,8 +1,13 @@
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import Podcast
 
 
-class Home(TemplateView):
+class Home(LoginView):
     template_name = 'home.html'
 
 
@@ -16,3 +21,29 @@ class PodcastDetail(DetailView):
     model = Podcast
     template_name = 'podcasts/detail.html'
     context_object_name = 'podcast'
+
+
+
+class LogoutUser(LogoutView):
+    next_page = 'home'
+
+def signup(request):
+    error_message = ''
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+
+    form = UserCreationForm()
+    context = {
+        'form': form,
+        'error_message': error_message
+    }
+
+    return render(request, 'signup.html', context)
