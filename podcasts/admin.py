dcb8284e642f61b6c3372044860de9c54432a9b7
levi81770 <levi77081@gmail.com>
2026-06-30 23:01:57 -0400

    Add podcast review and playlist models

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Podcast, Review, Playlist
 
-# Register your models here.
+
+admin.site.register(Podcast)
+admin.site.register(Review)
+admin.site.register(Playlist)
--- podcasts/models.py
+++ podcasts/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Podcast(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    host = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=500, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('podcast_detail', kwargs={'pk': self.pk})
+
+
+class Review(models.Model):
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.podcast.title} - {self.rating}/5'
+
+    def get_absolute_url(self):
+        return self.podcast.get_absolute_url()
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    podcasts = models.ManyToManyField(Podcast, blank=True, related_name='playlists')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('playlist_detail', kwargs={'pk': self.pk})
