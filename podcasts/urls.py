2e5869cd9d773ba8d41ba888950caf59e314a879
levi81770 <levi77081@gmail.com>
2026-06-30 23:47:22 -0400

    Add podcast CRUD

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('logout/', views.LogoutUser.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('podcasts/', views.PodcastList.as_view(), name='podcast_list'),
+    path('podcasts/create/', views.PodcastCreate.as_view(), name='podcast_create'),
     path('podcasts/<int:pk>/', views.PodcastDetail.as_view(), name='podcast_detail'),
+    path('podcasts/<int:pk>/update/', views.PodcastUpdate.as_view(), name='podcast_update'),
+    path('podcasts/<int:pk>/delete/', views.PodcastDelete.as_view(), name='podcast_delete'),
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Podcast
 
 
@@ -22,7 +24,23 @@ class PodcastDetail(DetailView):
     template_name = 'podcasts/detail.html'
     context_object_name = 'podcast'
 
+class PodcastCreate(LoginRequiredMixin, CreateView):
+    model = Podcast
+    fields = ['title', 'description', 'host', 'image_url']
+    template_name = 'podcasts/form.html'
 
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+class PodcastUpdate(LoginRequiredMixin, UpdateView):
+    model = Podcast
+    fields = ['title', 'description', 'host', 'image_url']
+    template_name = 'podcasts/form.html'
+
+class PodcastDelete(LoginRequiredMixin, DeleteView):
+    model = Podcast
+    success_url = reverse_lazy('podcast_list')
 
 class LogoutUser(LogoutView):
     next_page = 'home'
