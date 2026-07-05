65c32a90810726e7fdfbf9895ed209ace3177bab
levi81770 <levi77081@gmail.com>
2026-07-05 17:39:43 -0400

    Add and removed podcast to/from playlist functionality

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -2,15 +2,54 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
-    path('logout/', views.LogoutUser.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
-    path('podcasts/', views.PodcastList.as_view(), name='podcast_list'),
-    path('podcasts/create/', views.PodcastCreate.as_view(), name='podcast_create'),
-    path('podcasts/<int:pk>/', views.PodcastDetail.as_view(), name='podcast_detail'),
-    path('podcasts/<int:pk>/update/', views.PodcastUpdate.as_view(), name='podcast_update'),
-    path('podcasts/<int:pk>/delete/', views.PodcastDelete.as_view(), name='podcast_delete'),
-    path('podcasts/<int:pk>/reviews/create/', views.ReviewCreate.as_view(), name='review_create'),
-    path('reviews/<int:pk>/update/', views.ReviewUpdate.as_view(), name='review_update'),
-    path('reviews/<int:pk>/delete/', views.ReviewDelete.as_view(), name='review_delete'),
+    path("", views.Home.as_view(), name="home"),
+    path("logout/", views.LogoutUser.as_view(), name="logout"),
+    path("signup/", views.signup, name="signup"),
+    path("podcasts/", views.PodcastList.as_view(), name="podcast_list"),
+    path("podcasts/create/", views.PodcastCreate.as_view(), name="podcast_create"),
+    path("podcasts/<int:pk>/", views.PodcastDetail.as_view(), name="podcast_detail"),
+    path(
+        "podcasts/<int:pk>/update/",
+        views.PodcastUpdate.as_view(),
+        name="podcast_update",
+    ),
+    path(
+        "podcasts/<int:pk>/delete/",
+        views.PodcastDelete.as_view(),
+        name="podcast_delete",
+    ),
+    path(
+        "podcasts/<int:pk>/reviews/create/",
+        views.ReviewCreate.as_view(),
+        name="review_create",
+    ),
+    path(
+        "reviews/<int:pk>/update/", views.ReviewUpdate.as_view(), name="review_update"
+    ),
+    path(
+        "reviews/<int:pk>/delete/", views.ReviewDelete.as_view(), name="review_delete"
+    ),
+    path("playlists/", views.PlaylistList.as_view(), name="playlist_list"),
+    path("playlists/create/", views.PlaylistCreate.as_view(), name="playlist_create"),
+    path("playlists/<int:pk>/", views.PlaylistDetail.as_view(), name="playlist_detail"),
+    path(
+        "playlists/<int:pk>/update/",
+        views.PlaylistUpdate.as_view(),
+        name="playlist_update",
+    ),
+    path(
+        "playlists/<int:pk>/delete/",
+        views.PlaylistDelete.as_view(),
+        name="playlist_delete",
+    ),
+    path(
+        "podcasts/<int:podcast_id>/add_to_playlist/",
+        views.add_to_playlist,
+        name="add_to_playlist",
+    ),
+    path(
+        "playlists/<int:playlist_id>/remove/<int:podcast_id>/",
+        views.remove_from_playlist,
+        name="remove_from_playlist",
+    ),
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -1,74 +1,102 @@
-
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from .models import Podcast, Review
+from .models import Podcast, Review, Playlist
+from django.contrib.auth.decorators import login_required
 
 
 class Home(LoginView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
 
 class PodcastList(ListView):
     model = Podcast
-    template_name = 'podcasts/index.html'
-    context_object_name = 'podcasts'
+    template_name = "podcasts/index.html"
+    context_object_name = "podcasts"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context["playlists"] = Playlist.objects.filter(owner=self.request.user)
+
+        return context
 
 
 class PodcastDetail(DetailView):
     model = Podcast
-    template_name = 'podcasts/detail.html'
-    context_object_name = 'podcast'
+    template_name = "podcasts/detail.html"
+    context_object_name = "podcast"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context["playlists"] = Playlist.objects.filter(owner=self.request.user)
+
+        return context
+
 
 class PodcastCreate(LoginRequiredMixin, CreateView):
     model = Podcast
-    fields = ['title', 'description', 'host', 'image_url']
-    template_name = 'podcasts/form.html'
+    fields = ["title", "description", "host", "image_url"]
+    template_name = "podcasts/form.html"
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
+
 class PodcastUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Podcast
-    fields = ['title', 'description', 'host', 'image_url']
-    template_name = 'podcasts/form.html'
+    fields = ["title", "description", "host", "image_url"]
+    template_name = "podcasts/form.html"
 
     def test_func(self):
         return self.get_object().created_by == self.request.user
+
 
 class PodcastDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Podcast
-    success_url = reverse_lazy('podcast_list')
+    success_url = reverse_lazy("podcast_list")
 
     def test_func(self):
         return self.get_object().created_by == self.request.user
 
+
 class ReviewCreate(LoginRequiredMixin, CreateView):
     model = Review
-    fields = ['rating', 'comment']
-    template_name = 'podcasts/review_form.html'
+    fields = ["rating", "comment"]
+    template_name = "podcasts/review_form.html"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        form.instance.podcast_id = self.kwargs['pk']
+        form.instance.podcast_id = self.kwargs["pk"]
         return super().form_valid(form)
+
 
 class ReviewUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Review
-    fields = ['rating', 'comment']
-    template_name = 'podcasts/review_form.html'
+    fields = ["rating", "comment"]
+    template_name = "podcasts/review_form.html"
 
     def test_func(self):
         return self.get_object().user == self.request.user
 
+
 class ReviewDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Review
-    template_name = 'podcasts/review_confirm_delete.html'
+    template_name = "podcasts/review_confirm_delete.html"
 
     def test_func(self):
         return self.get_object().user == self.request.user
@@ -76,26 +104,101 @@ class ReviewDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def get_success_url(self):
         return self.object.podcast.get_absolute_url()
 
+
+class PlaylistList(LoginRequiredMixin, ListView):
+    model = Playlist
+    template_name = "podcasts/playlist_index.html"
+    context_object_name = "playlists"
+
+    def get_queryset(self):
+        return Playlist.objects.filter(owner=self.request.user)
+
+
+class PlaylistDetail(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Playlist
+    template_name = "podcasts/playlist_detail.html"
+    context_object_name = "playlist"
+
+    def test_func(self):
+        return self.get_object().owner == self.request.user
+
+
+class PlaylistCreate(LoginRequiredMixin, CreateView):
+    model = Playlist
+    fields = ["name", "description"]
+    template_name = "podcasts/playlist_form.html"
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class PlaylistUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Playlist
+    fields = ["name", "description"]
+    template_name = "podcasts/playlist_form.html"
+
+    def test_func(self):
+        playlist = self.get_object()
+        return playlist.owner == self.request.user
+
+
+class PlaylistDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Playlist
+    template_name = "podcasts/playlist_confirm_delete.html"
+    success_url = reverse_lazy("playlist_list")
+
+    def test_func(self):
+        playlist = self.get_object()
+        return playlist.owner == self.request.user
+
+
+@login_required
+def add_to_playlist(request, podcast_id):
+    if request.method == "POST":
+        podcast = get_object_or_404(Podcast, id=podcast_id)
+        playlist_id = request.POST.get("playlist_id")
+        playlist = get_object_or_404(Playlist, id=playlist_id, owner=request.user)
+
+    playlist.podcasts.add(podcast)
+
+    next_url = request.POST.get("next")
+    if next_url:
+        return redirect(next_url)
+
+    return redirect("podcast_list")
+
+
+@login_required
+def remove_from_playlist(request, playlist_id, podcast_id):
+    playlist = get_object_or_404(Playlist, id=playlist_id, owner=request.user)
+
+    podcast = get_object_or_404(Podcast, id=podcast_id)
+
+    if request.method == "POST":
+        playlist.podcasts.remove(podcast)
+
+    return redirect("playlist_detail", pk=playlist.id)
+
+
 class LogoutUser(LogoutView):
-    next_page = 'home'
+    next_page = "home"
+
 
 def signup(request):
-    error_message = ''
+    error_message = ""
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            error_message = 'Invalid sign up - try again'
+            error_message = "Invalid sign up - try again"
 
     form = UserCreationForm()
-    context = {
-        'form': form,
-        'error_message': error_message
-    }
+    context = {"form": form, "error_message": error_message}
 
-    return render(request, 'signup.html', context)
+    return render(request, "signup.html", context)
